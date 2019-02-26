d23cd9cd9f8ee50a9c730dea263cde9a8f1ca08b
Js-Guru321 <js.guru321@gmail.com>
2019-02-26 21:45:16 +0800

    milestone 3-1 Add heroes functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroListComponent } from './hero-list/hero-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'crisis-center', component: CrisisListComponent },
-  { path: 'heroes', component: HeroListComponent },
   // Default redirect route must be put above wildcard route.
   { path: '', redirectTo: '/heroes', pathMatch: 'full' }, 
   // wildcard route
