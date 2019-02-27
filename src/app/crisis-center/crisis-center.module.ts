bbd96c79417d3a27d5ba07ffdd922747d818255e
Js-Guru321 <js.guru321@gmail.com>
2019-02-28 02:46:45 +0800

    milestone 4-2 Child routing component

--- src/app/crisis-center/crisis-center.module.ts
+++ src/app/crisis-center/crisis-center.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { CrisisCenterRoutingModule } from './crisis-center-routing.module';
 import { CrisisDetailComponent } from './crisis-detail/crisis-detail.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
 
 @NgModule({
-  declarations: [CrisisDetailComponent, CrisisListComponent],
+  declarations: [CrisisDetailComponent, CrisisListComponent, CrisisCenterComponent],
   imports: [
     CommonModule,
     CrisisCenterRoutingModule
